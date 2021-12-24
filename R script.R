8e515a278fca76e204f96505270f8c3de8ff54b2
astroboy1183 <jayanthapalla@gmail.com>
2021-12-24 17:01:41 -0600

    Update R script.R

--- R script.R
+++ R script.R
@@ -1,1 +1,16 @@
-print('first commit')
+a<-b<-c<-22
+d<-33
+44->t
+print(d)
+print(t)
+
+x<-c(1,2,3,4,5)#c:combine/concatenate
+
+0:10
+10:0
+seq(10)
+seq(30,0,by=-3)
+sqrt(65)
+log(100) #log base e
+log10(100)
+
