6991e60344f4f981ad55a1a51888256a9f678fb4
astroboy1183 <jayanthapalla@gmail.com>
2021-12-24 17:53:32 -0600

    adding a second script file.

--- R script.R
+++ R script.R
@@ -22,3 +22,68 @@ typeof(l1)
 
 l2<-F
 typeof(l2)
+
+v<-c(1,2,3,4,5)#vector
+v
+v2<-c("r","a","d")
+v2
+is.vector(v2)
+v3<-c(TRUE,FALSE,TRUE)
+is.vector((v3))
+
+
+m1<-matrix(c(T,T,F,F,T,F,T,F),nrow=2)
+m1
+m2<-matrix(c(T,T,F,F,T,F,T,F),nrow=4)
+m2
+m3<-matrix(c(T,T,F,F,T,F,T,F),nrow=8)
+m3
+
+a1<-array(c(1:12),c(4,2)) #give data, then dimensions (rows,columns,tables)
+a1          
+
+a2<-array(c(1,12,2,4,5,33,44,5,7),c(3,3))
+a2
+
+
+
+#DataFrame
+
+
+vNumeric<-c(1,2,3)
+vCharacter<-c("a","b","c")
+vLogical<- c(T,F,T)
+
+df1<-cbind(vNumeric,vCharacter,vLogical)
+df2<-as.data.frame(cbind(vNumeric,vCharacter,vLogical))
+
+df1
+
+df2  #makes the dataframe columns in different datatypes
+
+l1<-c(1,2,3)
+l2<-c("a","b","c")
+l3<- c(T,F,T)
+
+l4 = list(l1)
+l5 = list(l2)
+l6 = list(l3)
+
+l4
+l5
+l6
+
+num<-5
+typeof(num)
+as.integer(num)
+
+mat = matrix(1:9,nrow=3)
+mat
+
+df<-as.data.frame(mat)
+df
+
+
+
+
+
