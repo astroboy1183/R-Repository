87c126dfc20b9a11bd8cc058ed5f0f87881191a0
astroboy1183 <jayanthapalla@gmail.com>
2021-12-25 17:53:34 -0600

    plots

--- R script2.R
+++ R script2.R
@@ -43,6 +43,137 @@ summary(UCBAdmissions)
 Titanic
 summary(Titanic)
 
-?swiss
+?swiss #info about the inbuilt dataset
 swiss
 summary(swiss)
+
+df<-read.csv('StateData.csv') #read csv file
+df
+
+(df <- import('StateData.xlsx') %>% as_tibble()) #read other formats using rio package.
+
+
+x=c(1,5,7,8,3,4,6)
+barplot(x)#creating a barplot
+
+?colors #information about colors
+colors() #names of colours
+
+browseURL('https://datalab.cc/rcolors') #goes to the particular URL
+
+barplot(x,col='red3')
+barplot(x,col='slategray3')
+
+barplot(x,col=rgb(0.80,0,0)) #red3
+barplot(x,col=rgb(.62,.71,.80)) #slategray3
+
+barplot(x,col=rgb(205,0,0,max=255)) #red3
+barplot(x,col=rgb(159,182,205,max=255))#slategray3
+
+barplot(x,col="#CD0000") #red3
+barplot(x,col="#9FB6CD")#slategray3
+
+#using index numbers
+barplot(x,col=colors()[555]) #red3
+barplot(x,col=colors()[602])#slategray3
+
+colors()
+
+
+barplot(x,col=c('red3','slategray3')) #red3
+
+?palette
+palette()        
+
+barplot(x,col=1:6)
+barplot(x,col = rainbow(6))
+barplot(x,col = heat.colors(6)) #for showing values in particular order.
+barplot(x,col = terrain.colors(6))
+barplot(x,col = topo.colors(6))
+barplot(x,col = cm.colors(6))
+
+?diamonds
+diamonds  #diamonds dataset
+
+?plot
+?barplot
+
+plot(diamonds$cut) #simplest bar plot
+
+#same plot using pipes
+diamonds %>%
+ select(cut)%>%
+ plot()  
+
+diamonds %>%
+  select(clarity)%>%
+  plot()  
+
+
+diamonds %>%
+  select(clarity)%>%
+  barplot() #error: height must be a vector or a matrix  
+
+diamonds %>%
+  select(clarity)%>%
+  table()%>% #put data in appropriate table form
+  barplot()  
+
+diamonds
+
+diamonds %>%
+  select(clarity,cut)%>%
+  plot()  
+
+
+#stacked bars:
+df<-diamonds %>%
+  select(color,clarity)%>%
+  table()%>%
+  print()
+
+df%>%
+  barplot(legend=rownames(.),beside = T) #stack plots beside each other
+
+
+df%>%
+  barplot(legend=rownames(.)) #plot with legend
+
+
+
+#histograms
+
+hist(diamonds$price)#default histogram
+
+hist(
+diamonds$price,breaks=7,col=colors()[566],xlab = 'price of diamonds',ylab='Frequency')
+
+
+
+#box plots
+
+?boxplot
+
+#Default
+boxplot(diamonds$price)
+
+#with parameters
+boxplot(diamonds$price,horizontal = TRUE,col=colors()[333],notch=TRUE
+        ,xlab="frequency")
+
+
+diamonds%>%
+  select(color,price)%>%
+  plot()
+
+
+diamonds%>%
+  select(color,price)%>%
+  boxplot(price~color,data=.
+  ,col=colors()[370])
+
+
+
+#scatterplots
+
+install.packages("readxl")
